94f7cec2693ad15748762a5e79595d16332ed14f
Anthony <anthonybuitrago8@gmail.com>
2026-07-19 19:01:50 -0400

    fix(ui): cambiar formulario de inicio a FrmPrincipal

--- HotelZormat/Program.cs
+++ HotelZormat/Program.cs
@@ -10,7 +10,7 @@ namespace HotelZormat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmGestionHabitaciones());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
